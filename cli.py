e1db8ea120d161483f7ad5de631205bcf22fe007
joshlacount <josh@lacount.dev>
2024-09-28 19:35:00 +0000

    proejct structure

---
+++ cli.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(f"{SCRIPT_DIR}/src")
+
+import nopy
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
